8dc9fee60d3aa126e933071278690055f6c3bcd6
HariBulgaria <haribohosyan25@itpg-varna.bg>
2024-07-05 11:19:34 +0300

    final commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             openFileDialog1 = new OpenFileDialog();
-            contentVisualizer = new ListBox();
             dragTextLabel = new Label();
             csvButton = new Button();
             errorLabel = new Label();
@@ -37,41 +36,41 @@
             folderBrowserDialog1 = new FolderBrowserDialog();
             browseFiles = new Button();
             textBox1 = new TextBox();
+            dialogSelectButton = new Button();
+            contentVisualizer = new RichTextBox();
+            SaveAsButton = new Button();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            errorLabel2 = new Label();
             SuspendLayout();
             // 
             // openFileDialog1
             // 
             openFileDialog1.FileName = "openFileDialog1";
             // 
-            // contentVisualizer
-            // 
-            contentVisualizer.FormattingEnabled = true;
-            contentVisualizer.ItemHeight = 15;
-            contentVisualizer.Location = new Point(12, 235);
-            contentVisualizer.Name = "contentVisualizer";
-            contentVisualizer.Size = new Size(760, 214);
-            contentVisualizer.TabIndex = 0;
-            contentVisualizer.Visible = false;
-            // 
             // dragTextLabel
             // 
             dragTextLabel.AutoSize = true;
             dragTextLabel.BackColor = Color.Transparent;
-            dragTextLabel.Font = new Font("Segoe UI", 39.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            dragTextLabel.Location = new Point(64, 161);
+            dragTextLabel.Font = new Font("Times New Roman", 39.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dragTextLabel.Location = new Point(82, 171);
             dragTextLabel.Name = "dragTextLabel";
-            dragTextLabel.Size = new Size(654, 71);
+            dragTextLabel.Size = new Size(612, 61);
             dragTextLabel.TabIndex = 1;
-            dragTextLabel.Text = "Drag your Excel file here.";
+            dragTextLabel.Text = "Drag your Excel file here:";
             // 
             // csvButton
             // 
-            csvButton.Location = new Point(685, 189);
+            csvButton.BackColor = Color.NavajoWhite;
+            csvButton.FlatStyle = FlatStyle.Popup;
+            csvButton.Font = new Font("Times New Roman", 12F);
+            csvButton.Location = new Point(108, 189);
             csvButton.Name = "csvButton";
             csvButton.Size = new Size(87, 35);
             csvButton.TabIndex = 2;
             csvButton.Text = "Make .csv";
-            csvButton.UseVisualStyleBackColor = true;
+            csvButton.UseVisualStyleBackColor = false;
             csvButton.Visible = false;
             csvButton.Click += csvButton_Click;
             // 
@@ -79,23 +78,26 @@
             // 
             errorLabel.AutoSize = true;
             errorLabel.BackColor = Color.Transparent;
-            errorLabel.Font = new Font("Segoe UI", 15F);
-            errorLabel.ForeColor = Color.IndianRed;
-            errorLabel.Location = new Point(222, 232);
+            errorLabel.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            errorLabel.ForeColor = Color.Firebrick;
+            errorLabel.Location = new Point(229, 232);
             errorLabel.Name = "errorLabel";
-            errorLabel.Size = new Size(365, 28);
+            errorLabel.Size = new Size(344, 22);
             errorLabel.TabIndex = 3;
             errorLabel.Text = "The file must be in a .xls or a .xlsx format!";
             errorLabel.Visible = false;
             // 
             // txtButton
             // 
-            txtButton.Location = new Point(685, 148);
+            txtButton.BackColor = Color.NavajoWhite;
+            txtButton.FlatStyle = FlatStyle.Popup;
+            txtButton.Font = new Font("Times New Roman", 12F);
+            txtButton.Location = new Point(201, 189);
             txtButton.Name = "txtButton";
             txtButton.Size = new Size(87, 35);
             txtButton.TabIndex = 4;
             txtButton.Text = "Make .txt";
-            txtButton.UseVisualStyleBackColor = true;
+            txtButton.UseVisualStyleBackColor = false;
             txtButton.Visible = false;
             txtButton.Click += txtButton_Click;
             // 
@@ -105,34 +107,133 @@
             // 
             // browseFiles
             // 
+            browseFiles.BackColor = Color.NavajoWhite;
+            browseFiles.FlatStyle = FlatStyle.Popup;
+            browseFiles.Font = new Font("Times New Roman", 10F);
             browseFiles.Location = new Point(12, 12);
             browseFiles.Name = "browseFiles";
             browseFiles.Size = new Size(75, 23);
             browseFiles.TabIndex = 5;
             browseFiles.Text = "Browse";
-            browseFiles.UseVisualStyleBackColor = true;
+            browseFiles.UseVisualStyleBackColor = false;
             browseFiles.Click += browseFiles_Click;
             // 
             // textBox1
             // 
+            textBox1.BackColor = Color.Linen;
+            textBox1.Cursor = Cursors.IBeam;
             textBox1.Location = new Point(108, 12);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(479, 23);
             textBox1.TabIndex = 6;
             // 
+            // dialogSelectButton
+            // 
+            dialogSelectButton.BackColor = Color.NavajoWhite;
+            dialogSelectButton.FlatStyle = FlatStyle.Popup;
+            dialogSelectButton.Font = new Font("Times New Roman", 10F);
+            dialogSelectButton.Location = new Point(593, 12);
+            dialogSelectButton.Name = "dialogSelectButton";
+            dialogSelectButton.Size = new Size(75, 23);
+            dialogSelectButton.TabIndex = 7;
+            dialogSelectButton.Text = "Select";
+            dialogSelectButton.UseVisualStyleBackColor = false;
+            dialogSelectButton.Click += button1_Click;
+            // 
+            // contentVisualizer
+            // 
+            contentVisualizer.BackColor = Color.Linen;
+            contentVisualizer.Location = new Point(12, 235);
+            contentVisualizer.Name = "contentVisualizer";
+            contentVisualizer.Size = new Size(760, 214);
+            contentVisualizer.TabIndex = 8;
+            contentVisualizer.Text = "";
+            contentVisualizer.Visible = false;
+            // 
+            // SaveAsButton
+            // 
+            SaveAsButton.BackColor = Color.NavajoWhite;
+            SaveAsButton.FlatStyle = FlatStyle.Popup;
+            SaveAsButton.Font = new Font("Times New Roman", 12F);
+            SaveAsButton.Location = new Point(12, 189);
+            SaveAsButton.Name = "SaveAsButton";
+            SaveAsButton.Size = new Size(87, 35);
+            SaveAsButton.TabIndex = 9;
+            SaveAsButton.Text = "Save As";
+            SaveAsButton.UseVisualStyleBackColor = false;
+            SaveAsButton.Visible = false;
+            SaveAsButton.Click += SaveAsButton_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.Linen;
+            textBox3.Cursor = Cursors.IBeam;
+            textBox3.Location = new Point(108, 160);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(134, 23);
+            textBox3.TabIndex = 12;
+            textBox3.Visible = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Times New Roman", 11F);
+            label1.Location = new Point(31, 160);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 17);
+            label1.TabIndex = 13;
+            label1.Text = "File name:";
+            label1.Visible = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Times New Roman", 11F);
+            label2.ForeColor = Color.DarkSlateGray;
+            label2.Location = new Point(308, 207);
+            label2.Name = "label2";
+            label2.Size = new Size(320, 17);
+            label2.TabIndex = 14;
+            label2.Text = "Your document can be found in the downloads folder.";
+            label2.Visible = false;
+            // 
+            // errorLabel2
+            // 
+            errorLabel2.AutoSize = true;
+            errorLabel2.BackColor = Color.Transparent;
+            errorLabel2.Font = new Font("Times New Roman", 10F);
+            errorLabel2.ForeColor = Color.Firebrick;
+            errorLabel2.Location = new Point(108, 141);
+            errorLabel2.Name = "errorLabel2";
+            errorLabel2.Size = new Size(35, 16);
+            errorLabel2.TabIndex = 15;
+            errorLabel2.Text = "error";
+            errorLabel2.Visible = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PapayaWhip;
             ClientSize = new Size(784, 461);
+            Controls.Add(errorLabel2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(SaveAsButton);
+            Controls.Add(contentVisualizer);
+            Controls.Add(dialogSelectButton);
             Controls.Add(textBox1);
             Controls.Add(browseFiles);
             Controls.Add(txtButton);
             Controls.Add(errorLabel);
             Controls.Add(csvButton);
             Controls.Add(dragTextLabel);
-            Controls.Add(contentVisualizer);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Form1";
+            RightToLeft = RightToLeft.No;
             Text = "Form1";
             Load += Form1_Load;
             DragDrop += Form1_DragDrop;
@@ -144,7 +245,6 @@
         #endregion
 
         private OpenFileDialog openFileDialog1;
-        private ListBox contentVisualizer;
         private Label dragTextLabel;
         private Button csvButton;
         private Label errorLabel;
@@ -152,5 +252,12 @@
         private FolderBrowserDialog folderBrowserDialog1;
         private Button browseFiles;
         private TextBox textBox1;
+        private Button dialogSelectButton;
+        private RichTextBox contentVisualizer;
+        private Button SaveAsButton;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label errorLabel2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,17 @@
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using Spire.Xls;
 using System.Text;
+using static System.Windows.Forms.LinkLabel;
 
 namespace CSVConverterApp
 {
     public partial class Form1 : Form
     {
-        string filePath = "C:\\Users\\Students\\Documents\\output.csv"; //To make custom    
+        string filePath = "C:\\Users\\Students\\Documents\\output.csv"; //To make custom
+        string fileLoc = "";
+        List<string> txtLines = new List<string>();
+        bool ender = false;
+        string downloadsFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
         public Form1()
         {
             InitializeComponent();
@@ -22,46 +27,52 @@ namespace CSVConverterApp
 
         private void Form1_DragDrop(object sender, DragEventArgs e)
         {
-            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-
-            if (files.Length > 0)
+            if (!ender)
             {
-                string file = files[0];
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
 
-                if (IsValidExcelFile(file))
+                if (files.Length > 0)
                 {
-                    contentVisualizer.Visible = true;
-                    csvButton.Visible = true;
-                    txtButton.Visible = true;
-                    dragTextLabel.Visible = false;
-                    errorLabel.Visible = false;
-                    Workbook workbook = new Workbook();
-                    workbook.LoadFromFile(file);
-                    Worksheet sheet = workbook.Worksheets[0];
-                    sheet.SaveToFile("ExceltoTxt.txt", ",", Encoding.UTF8);
-                    using (StreamReader sr = new StreamReader("ExceltoTxt.txt"))
+                    fileLoc = files[0];
+
+                    if (IsValidExcelFile(fileLoc))
                     {
-                        string line = "";
-                        while (true)
+                        contentVisualizer.Visible = true;
+                        dragTextLabel.Visible = false;
+                        SaveAsButton.Visible = true;
+                        errorLabel.Visible = false;
+                        textBox1.Enabled = false;
+                        browseFiles.Enabled = false;
+                        dialogSelectButton.Enabled = false;
+                        Workbook workbook = new Workbook();
+                        workbook.LoadFromFile(fileLoc);
+                        Worksheet sheet = workbook.Worksheets[0];
+                        sheet.SaveToFile("ExceltoTxt.txt", ",", Encoding.UTF8);
+                        using (StreamReader sr = new StreamReader("ExceltoTxt.txt"))
                         {
-                            if ((line = sr.ReadLine()) != null)
+                            string line = "";
+                            while (true)
                             {
-                                contentVisualizer.Items.Add(line);
-                            }
-                            else
-                            {
-                                break;
+                                if ((line = sr.ReadLine()) != null)
+                                {
+                                    txtLines.Add(line);
+                                }
+                                else
+                                {
+                                    break;
+                                }
                             }
                         }
+                        ender = true;
                     }
-                }
-                else
-                {
-                    errorLabel.Visible = true;
-                }
-                for (int i = 0; i < contentVisualizer.Items.Count / 2; i++)
-                {
-                    contentVisualizer.Items.RemoveAt(0);
+                    else
+                    {
+                        errorLabel.Visible = true;
+                    }
+                    foreach (var line in txtLines)
+                    {
+                        contentVisualizer.Text += line + "\n";
+                    }
                 }
             }
         }
@@ -94,31 +105,64 @@ namespace CSVConverterApp
 
         private void csvButton_Click(object sender, EventArgs e)
         {
-            using (StreamWriter sw = new StreamWriter(filePath))
+            try
             {
-                foreach (var row in contentVisualizer.Items)
+                if (textBox3.Text == string.Empty) throw new NullNameException();
+                txtLines.Clear();
+                txtLines = contentVisualizer.Text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                using (StreamWriter sw = new StreamWriter($"{downloadsFolderPath}\\{textBox3.Text}.csv"))
                 {
-                    if (row == null)
+                    foreach (var row in txtLines)
                     {
-                        break;
+                        if (row == null)
+                        {
+                            break;
+                        }
+                        sw.WriteLine(row);
                     }
-                    sw.WriteLine(row);
                 }
+                errorLabel2.Visible = false;
+            }
+            catch (NullNameException)
+            {
+                errorLabel2.Text = "File name must not be empty/File not found!";
+                errorLabel2.Visible = true;
+            }
+            catch (Exception)
+            {
+                errorLabel2.Text = "An error has occurred. Try again.";
+                errorLabel2.Visible = true;
             }
         }
-
         private void txtButton_Click(object sender, EventArgs e)
         {
-            using (StreamWriter sw = new StreamWriter(filePath))
+            try
             {
-                foreach (var row in contentVisualizer.Items)
+                if (textBox3.Text == string.Empty) throw new NullNameException();
+                txtLines.Clear();
+                txtLines = contentVisualizer.Text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                using (StreamWriter sw = new StreamWriter($"{downloadsFolderPath}\\{textBox3.Text}.txt"))
                 {
-                    if (row == null)
+                    foreach (var row in txtLines)
                     {
-                        break;
+                        if (row == null)
+                        {
+                            break;
+                        }
+                        sw.WriteLine(row);
                     }
-                    sw.WriteLine(row);
                 }
+                errorLabel2.Visible = false;
+            }
+            catch (NullNameException)
+            {
+                errorLabel2.Text = "File name must not be empty/File not found!";
+                errorLabel2.Visible = true;
+            }
+            catch (Exception)
+            {
+                errorLabel2.Text = "An error has occurred. Try again.";
+                errorLabel2.Visible = true;
             }
         }
 
@@ -134,6 +178,59 @@ namespace CSVConverterApp
             {
                 textBox1.Text = fdlg.FileName;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!ender)
+            {
+                if (IsValidExcelFile(textBox1.Text))
+                {
+                    fileLoc = textBox1.Text;
+                    contentVisualizer.Visible = true;
+                    dragTextLabel.Visible = false;
+                    SaveAsButton.Visible = true;
+                    errorLabel.Visible = false;
+                    textBox1.Enabled = false;
+                    Workbook workbook = new Workbook();
+                    workbook.LoadFromFile(textBox1.Text);
+                    Worksheet sheet = workbook.Worksheets[0];
+                    sheet.SaveToFile("ExceltoTxt.txt", ",", Encoding.UTF8);
+                    using (StreamReader sr = new StreamReader("ExceltoTxt.txt"))
+                    {
+                        string line = "";
+                        while (true)
+                        {
+                            if ((line = sr.ReadLine()) != null)
+                            {
+                                txtLines.Add(line);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    ender = true;
+                }
+                else
+                {
+                    errorLabel.Visible = true;
+                }
+                foreach (var line in txtLines)
+                {
+                    contentVisualizer.Text += line + "\n";
+                }
+            }
+        }
+
+        private void SaveAsButton_Click(object sender, EventArgs e)
+        {
+            csvButton.Visible = true;
+            txtButton.Visible = true;
+            textBox3.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
         }
     }
 }
